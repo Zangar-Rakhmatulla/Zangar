88e99121a141ab52188127f23d1eedb325ea6daf
Zangar <rakhmatulla.zangar@mail.ru>
2025-02-12 09:35:21 +0500

    hw_2

---
+++ HW_2/hw_2/blog/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.CharField(max_length=255)
+    author = models.CharField(max_length=100)
+    
